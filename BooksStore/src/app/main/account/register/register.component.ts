e3665ae3497b08578e9ebebebfa038ba0d2fba50
NechamaShraiber <Nechama.Shraiber@seldatinc.com>
2018-09-02 12:55:03 +0300

    dsa

--- BooksStore/src/app/main/account/register/register.component.ts
+++ BooksStore/src/app/main/account/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component,  } from '@angular/core';
-import { FormGroup, FormControl, ValidatorFn } from '@angular/forms';
+import { FormGroup, FormControl,AbstractControl, ValidatorFn } from '@angular/forms';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -10,10 +10,10 @@ export class RegisterComponent   {
   obj: typeof Object = Object;
   constructor() { 
     let formGroupConfig = {
-     FirstName: new FormControl("Anonimy", this.createValidatorArr("FirstName", 2, 15),),
-     LastName: new FormControl("Anonimy", this.createValidatorArr("LastName", 2, 15)),
-     UserName: new FormControl("Anonimy", this.createValidatorArr("UserName", 4, 15)),
-      Password: new FormControl("12345", this.createValidatorArr("password", 5, 10))
+    FirstName: new FormControl("Anonimy", this.createValidatorArr("FirstName", 2, 15, /^[a-zA-Z]*$/), ),
+     LastName: new FormControl("Anonimy", this.createValidatorArr("LastName", 2, 15,/^[a-zA-Z]*$/)),
+     UserName: new FormControl("Anonimy", this.createValidatorArr("UserName", 4, 15,/^[a-zA-Z]*$/)),
+    Password: new FormControl("12345", this.createValidatorArr("password", 5, 10,/^[a-zA-Z0-9]*$/ ))
     };
 
     this.formGroup = new FormGroup(formGroupConfig);
@@ -24,12 +24,17 @@ export class RegisterComponent   {
     console.log(this.formGroup.controls);
     alert(this.formGroup.status)
   }
-  createValidatorArr(cntName: string, min: number, max: number): Array<ValidatorFn> {
+//   pattern(cntName: string) : Array<ValidatorFn> {
+//     return [f=>f.value&& <string>f.value.match(/a-zA-Z/) ? { "val": `${cntName} is  english` } : null]
+// }
+  createValidatorArr(cntName: string, min: number, max: number, reg: RegExp): Array<ValidatorFn> {
     return [
       f => !f.value ? { "val": `${cntName} is required` } : null,
       f => f.value && f.value.length > max ? { "val": `${cntName} is max ${max} chars` } : null,
       f => f.value && f.value.length < min ? { "val": `${cntName} is min ${min} chars` } : null,
-      // f=>f.value &&f.value.pattern:'a-zA-Z' ? { "val": `${cntName} is only english letters` } : null,
+      f=>f.value&& !<string>f.value.match(reg) ?cntName=='password'?{ "val": `${cntName} must contains  english letters or numbers` }:
+       { "val": `${cntName} must contains only english letters` } : null
+   
     ];
   }
 }
