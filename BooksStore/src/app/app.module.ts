1a347514724a9a95777a2de21e4d94f55d220db3
NechamaShraiber <42530703+NechamaShraiber@users.noreply.github.com>
2018-09-02 09:51:28 +0300

    fds

--- BooksStore/src/app/app.module.ts
+++ BooksStore/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppHeaderComponent } from './components/app-header/app-header.component';
+import { AppMainComponent } from './components/app-main/app-main.component';
+import { AppFooterComponent } from './components/app-footer/app-footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppHeaderComponent,
+    AppMainComponent,
+    AppFooterComponent
   ],
   imports: [
     BrowserModule
