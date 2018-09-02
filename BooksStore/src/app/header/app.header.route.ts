31af5d28f386019aaf1285847135dcefe26f12af
NechamaShraiber <42530703+NechamaShraiber@users.noreply.github.com>
2018-09-02 10:49:36 +0300

    10.49

---
+++ BooksStore/src/app/header/app.header.route.ts
@@ -0,0 +1,12 @@
+import { Routes, RouterModule } from '@angular/router';
+
+import {} from 
+ import { AuthGuard } from './shared/auth.guard';//vhjhvjvj
+ const appRoutes: Routes = [
+     { path: 'AuthRout/home', component: HomeComponent, canActivate: [AuthGuard] },
+     { path: 'AuthRout', redirectTo: 'AuthRout/home',},
+     { path: 'AuthRout/login', component: LoginComponent },
+     // otherwise redirect to home
+     { path: '**', redirectTo: 'AuthRout' }
+ ];
+ export const routing = RouterModule.forRoot(appRoutes);
